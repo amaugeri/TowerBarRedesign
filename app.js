159ba4ca34e8505136931b5d9b720cf3622fefbe
Alex Maugeri <amaugeri@ucsd.edu>
2017-09-18 22:20:02 -0700

    layout for branding and navbar

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require('path');
 
 var app = express();
 
@@ -13,6 +14,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 // set static path
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', function(req,res){
+	res.send("hello world");
+});
 
 app.listen(3000,function(){
 	console.log("server started");
